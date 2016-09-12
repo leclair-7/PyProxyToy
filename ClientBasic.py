40c1a5a1daeb72785937d66f2ea9db9608d1624f
wolfgang <leh140230@utdallas.edu>
2016-09-12 11:15:56 -0500

    Make client work with proxy server

--- ClientBasic.py
+++ ClientBasic.py
@@ -2,16 +2,19 @@
 
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "127.0.0.1" # server address
-port =20603 #server port
+host = "192.168.99.101" # server address
+port =20610 #server port
 s.connect((host,port))
 
 index = 25
 while True:
 	index += 1
-	print s.recv(1024)	
-	str1 = raw_input("insert next thing to send")
-	s.send( str1+": " + str(index))
+	fromServ =  s.recv(1024)
+	print fromServ
+	if fromServ == "exit":
+		break
+	str1 = raw_input("insert next thing to send: ")
+	s.send( str1)
 	
 
 	if index > 50:
