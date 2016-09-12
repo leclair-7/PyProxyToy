c04b7bb9e035dff3b4e116eb6710bb91ffa5149a
Wolfgang <wolf_leclair7@tutanota.com>
2016-09-12 14:45:57 -0500

    Make proxy forward message to and from client-server

--- BasicPyServer.py
+++ BasicPyServer.py
@@ -1,8 +1,6 @@
-
-
 import socket
-host = "127.0.0.1" #Server address
-port = 20603 #Port of Server
+host = "192.168.99.101" #Server address
+port = 20602 #Port of Server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host,port)) #bind socket to network addr
 s.listen(2)
@@ -15,43 +13,15 @@ print "Server is on ", conn, addr
 
 index = 0
 str1 = "blankNow"
-
-proxStr = ""
-
 while str1 != "exit":
 	index += 1
 	#print addr, "Now Connected"
-	conn.send("From server index number is: " + str(index))
-
-	#blocks/pauses here
+	conn.send("From End server index #: " + str(index) +" BAM!")
 	str1 = conn.recv(1024)
-	if str1.split()[0].lower() == "pserver":
-		conn.send("Enter <IPv4,port> to access from proxy:")
-		proxStr = conn.recv(1024)
-		while len(proxStr.split()) != 2:
-			conn.send("Enter <IPv4,port> to access from proxy:")
-			proxyStr = conn.recv(1024)
-		p_host = proxyStr[0] # server address
-		p_port = int(proxStr[1]) #server port
-		proxy_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		proxy_sock.connect((p_host,p_port))
-
-		msg_from_dest = ""
-
-		backToStart = ""
-
-		while msg_from_dest != "exit":
-			proxy_sock.send("online,...")
-			msg_from_dest = proxy_sock.recv(1024)
-			conn.send(msg_from_dest)
-
-
-
-
-		#now we'll access a server from a client socket
-
+	
 	print str1
 
 	if index > 25:
 		break
+conn.send("exit")
 conn.close()
--- ProxyServer.py
+++ ProxyServer.py
@@ -1,7 +1,5 @@
-
-
 import socket
-host = "127.0.0.2" #Server address
+host = "192.168.99.101" #Server address
 port = 20610 #Port of Server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host,port)) #bind socket to network addr
@@ -11,17 +9,48 @@ s.listen(2)
 #addr is IP/Port numbers
 conn, addr = s.accept()
 
-print "Server is on ", conn, addr
-
+#print "Server is on ", conn, addr
 index = 0
 str1 = "blankNow"
+
+isProxyOn = False
+sockToDest = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 while str1 != "exit":
 	index += 1
 	#print addr, "Now Connected"
-	conn.send("From server index number is: " + str(index))
+	conn.send("From middle server, msg#: " + str(index))
 	str1 = conn.recv(1024)
-	print str1
+
+	''' good part starts here'''
+
+	# See if client sent right signal to use the proxy to go someplace else
+	if isProxyOn is False and str1.split() != [] and str1.split()[0] == "prox" and len(str1.split()) is 3:
+		dest_ip = str1.split()[1]
+		try:		
+			dest_port = int( str1.split()[2] )			
+			#now have dest_ip, dest_port
+			
+			print "ready for next hop"
+			print( "[+] - Attempting connection with wanted destination" )
+			sockToDest.connect((dest_ip,dest_port))
+			isProxyOn = True
+			print "[+] - We have proxy to end server connectivity"					
+			
+		except Exception:
+			print "bad port number"
+	elif isProxyOn is True:
+		''' Potential for weirdness due to blocking errors'''		
+		sockToDest.send(str1)
+		msg_from_end = sockToDest.recv(1024)
+		conn.send(msg_from_end)
+	else:
+		print str1	
+		
+		
+	#print str1
 
 	if index > 25:
 		break
+conn.send("exit")
 conn.close()
+sockToDest.close()
